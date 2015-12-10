f5cec3fcfb60ad49f65501ad6d70fa89e996ca3e
calum1904 <chambe90@uni.coventry.ac.uk>
2015-12-10 08:25:54 +0000

    Update Tic Tac Toe Server.py

--- Tic Tac Toe Server.py
+++ Tic Tac Toe Server.py
@@ -22,34 +22,34 @@ class TicTacToe:
 			if conn is self.server: 						# new client connecting
 				c, addr = conn.accept()
 				
-				if addr not in self.connections:
-					self.connections.append(c) # Add client to list of connections
+				if addr not in self.connections:				#if the client isn't in the list
+					self.connections.append(c) 				# Add client to list of connections
 					print("\nINFO:: Got connection from" + str(addr) +"\nINFO:: Adding to client list")
-					if len(self.connections) == 1: # If there is 1 connection send a 'X' to the last added connection
+					if len(self.connections) == 1: 				# If there is 1 connection send a 'X' to the last added connection
 						assignTurn = "X"
 						self.connections[0].send(assignTurn.encode('utf-8'))
-					elif len(self.connections) == 2: # If there is 2 connections send a 'O' to the last added connection
+					elif len(self.connections) == 2: 			# If there is 2 connections send a 'O' to the last added connection
 						assignTurn = "O"
 						self.connections[1].send(assignTurn.encode('utf-8'))
 				
 
 			else:
-				msgbytes = conn.recv(1024) #Recieve a number from 0-8 defining which square was picked
-				msgbytes = msgbytes.decode('utf-8').rstrip('\r\n')
+				msgbytes = conn.recv(1024) 					#Recieve a number from 0-8 defining which square was picked
+				msgbytes = msgbytes.decode('utf-8').rstrip('\r\n')		#Decode the message
 				print ("INFO:: Square " + str(msgbytes) + " was played")
 				print ("INFO:: Sending that move to the other client...")
-				for client in self.connections: #Send the number to the other clients
-					if conn != client:
-						client.send(msgbytes.encode('utf-8'))
+				for client in self.connections: 				#Send the number to the other clients
+					if conn != client:					#But don't send it back to it self
+						client.send(msgbytes.encode('utf-8'))		#encode the message
 						print("INFO:: Move sent to the other client\n")
 				
 
-				if not msgbytes: 						# Treat an empty message as a disconnection
-					print('Disconnected')
+				if not msgbytes: 						# if the server receives an empty message
+					print('Disconnected')					# then disconnect
 
 
 #--------------------------------------------------------------------------Start the server---------------------------------------------------------------------------------
-
+#Prints to tell the user that the server has started and the ip address it is running on
 print("INFO:: Starting Tic Tac Toe Server")
 print("INFO:: The Servers ip is " + socket.gethostbyname(socket.gethostname()))
 print("INFO:: Binding to port: 12345\n")
